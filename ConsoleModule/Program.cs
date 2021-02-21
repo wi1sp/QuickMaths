4543bb59afce7be455100e15f574aeffa9e5c7b2
AlexBim <centuriin@yandex.ru>
2021-02-21 15:58:56 +0300

    f1

---
+++ ConsoleModule/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+            Console.WriteLine("Hello, Wrld");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Age is {age}");
+            Console.ReadKey();
+        }
+    }
+}
